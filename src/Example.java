318357ea6349c1786d4753aae309220a40d21a59
test <test@gmail.com>
2017-01-25 11:09:42 +0200

    prepare eclipse project

---
+++ src/Example.java
@@ -0,0 +1,23 @@
+public class Example { // inherited class Object
+   private int number;
+
+        public void setNumber(int number)   {
+            this.number = number;
+        }
+
+        public void hello(String text)   {
+            System.out.println(text + " from " + String.valueOf(number));
+        }
+
+        public static void main(String[ ] args)   {
+            Example example = new Example();
+            Example example2 = new Example();
+            //
+            example.setNumber(1);
+            example2.setNumber(2);
+            //
+            example.hello("My first program");
+            example2.hello("My first program");
+        }
+
+    }
