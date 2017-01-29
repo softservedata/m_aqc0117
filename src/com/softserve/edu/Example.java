e5281b14be609f30567c9cada2761d4582eb1f32
Sasha Stretovych <sasha.stretovych@gmail.com>
2017-01-29 17:21:37 +0200

    added comments and fixed bug

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -4,25 +4,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Example { // inherited class Object
-		private static int inputLength = 3;
+		private static int inputLength = 3; //const number that we need to read from console
 
         public static void main(String[ ] args) throws IOException   {
-           
+        	
+           // class that works with int array
             Numbers numbers = new Numbers();
+            //buffer reader to read from console
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int intBuffer;
             int counter = 0;
             while (counter < inputLength){
-	            System.out.print("Enter Integer or type Q to exit:");
-	            
+	            System.out.println("Enter Integer or type Q to exit:");
+	            //check if user wants to quit
 	            String s = br.readLine().toLowerCase();
 	        	if (s.equals("q")){
 	        		System.out.println("Exit");
 	        		break;
 	        	}
 	        	else{
+	        		//try to get int number
 		            try{
-		            	intBuffer = Integer.parseInt(br.readLine());
+		            	intBuffer = Integer.parseInt(s);
 		            	numbers.addNumber(intBuffer);
 		            	counter++;
 		            }catch(NumberFormatException nfe){
@@ -30,9 +33,13 @@ public class Example { // inherited class Object
 		            }
 	        	}
             }
+            //if all numbers were entered
             if (counter == inputLength){
+            	//show original
 	            numbers.printNumbers();
+	            //sort
 	            numbers.sortNumbers();
+	            //print
 	            numbers.printNumbers();
             }
             
