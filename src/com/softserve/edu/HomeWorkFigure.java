fd84974f9c85a3e1dbba65d15811248f0c2d184a
ryatskiv <roman.yatskiv@gmail.com>
2017-01-31 21:02:28 +0000

    Switches in file

--- src/com/softserve/edu/HomeWorkFigure.java
+++ src/com/softserve/edu/HomeWorkFigure.java
@@ -2,4 +2,64 @@ package com.softserve.edu;
 
 public class HomeWorkFigure {
 
+	public int length;
+	public int width;
+	public int perimetr;
+	
+	public HomeWorkFigure (String type) {
+		System.out.println("Figure type is " + type);
+	}
+		
+	
+	public void setLength(int lenght) {
+		this.length = lenght;
+	}
+
+	public int getLength() {
+		System.out.println("Lenght is " + length);
+		return length;
+	}
+	
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getWidth() {
+		System.out.println("Width is " + width);
+		return width;
+	}
+	
+	public int getPerimetr() {
+		perimetr = width * length;
+//		System.out.println("Perimetr is " + (width * length));
+		System.out.println("Perimetr is " + perimetr);
+//		perimetr = width * length;
+		return perimetr;
+	}
+	
+	public static void main (String args[]) {
+		System.out.println("Line");
+		
+		HomeWorkFigure rectangle = new HomeWorkFigure("Rectangle");
+		HomeWorkFigure square = new HomeWorkFigure("Square");
+	
+		square.setLength( 3 );
+		rectangle.setLength( 4 );
+		
+		square.setWidth( 6 );
+		rectangle.setWidth( 2 );
+				
+		square.getLength( );
+		rectangle.getLength( );
+		
+		square.getWidth( );
+		rectangle.getWidth( );
+				
+//		int qwe = square.getPerimetr( );
+//		System.out.println("qwe " + qwe);
+		square.getPerimetr( );
+		rectangle.getPerimetr( );
+	}
+	
 }
