f3397aa2610aa14c4fccfb156cdb90703b02d411
ryatskiv <roman.yatskiv@gmail.com>
2017-02-03 15:58:09 +0000

    HW2 in progress

--- src/com/softserve/edu/homework2/Appl.java
+++ src/com/softserve/edu/homework2/Appl.java
@@ -1,33 +1,17 @@
 package com.softserve.edu.homework2;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-
 public class Appl {
 
 	public static void main(String[] args) {
-		
-		
+
 		Square square = new Square();
 		Rectangle rectangle = new Rectangle();
 
 		rectangle.druk("");
 		square.druk("");
 
-		rectangle.perimeter(12, 54);
-		square.perimeter(2, 5);
-
-		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		rectangle.perimeter(length, width, total)
-		
-		
-		
-
-//		System.out.println("Enter String");
-//		String s = br.readLine();
-//		System.out.println("Enter Integer:");
-
-		// System.out.println("How" + square.druk(""));
+		rectangle.perimeter();
+		square.perimeter();
+	
 	}
 }
--- src/com/softserve/edu/homework2/Figure.java
+++ src/com/softserve/edu/homework2/Figure.java
@@ -1,12 +1,28 @@
 package com.softserve.edu.homework2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Figure {
 
-//	public int length;
-//	public int width;
+	private int length;
+	private int width;
 	private int total;
-	
-	public int perimeter(int length, int width) {
+
+	public int perimeter() {
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.print("Set lenght=");
+			length = Integer.parseInt(br.readLine());
+			System.out.print("Set width=");
+			width = Integer.parseInt(br.readLine());
+		
+		} catch (Exception e) { // NumberFormatException
+			System.out.println("I/O Error. e=" + e.toString());
+		}
+
 		total = (width + length) * 2;
 		System.out.println("Perimeter is " + total);
 		return total;
