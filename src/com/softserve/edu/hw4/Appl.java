4aabba1074f2b167e32c931bc38a96af4103014a
Sasha Stretovych <sasha.stretovych@gmail.com>
2017-02-07 23:32:11 +0200

    added hw4

---
+++ src/com/softserve/edu/hw4/Appl.java
@@ -0,0 +1,26 @@
+package com.softserve.edu.hw4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class Appl {
+	public static void main(String[] args) {
+
+		int[] array1 = new int[]{1,3,2,4,5,3,4,5};
+		int[] array2 = new int[]{1,2,3,4,5,3,5,4,};
+		System.out.println(Arrays.toString(array1));
+		System.out.println(Arrays.toString(array2));
+		System.out.println("Sort first -> ");
+		Arrays.sort(array1);
+		Arrays.sort(array2);
+		System.out.println(Arrays.toString(array1));
+		System.out.println(Arrays.toString(array2));
+		System.out.println("Using hashcode -> " + Arrays.hashCode(array1) + " and " + Arrays.hashCode(array2) + " are equal ? " + (Arrays.hashCode(array2) == Arrays.hashCode(array1)));
+
+		System.out.println("And using equals -> " + Arrays.equals(array1, array2));
+
+	}
+
+}
