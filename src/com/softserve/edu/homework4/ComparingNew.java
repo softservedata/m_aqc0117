4ef6ad06d73f2979398eb2aa310219e507cf5268
starter <starter@training.local>
2017-02-08 11:35:45 +0200

    hw4 correction

--- src/com/softserve/edu/homework4/ComparingNew.java
+++ src/com/softserve/edu/homework4/ComparingNew.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 public class ComparingNew {
 
 	private static boolean compareArrays(int[] a, int[] b) {
+		boolean key = true;
 		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i] == b[i]);
+			key = key && (a[i] == b[i]);
+			// System.out.println(a[i] == b[i]);
 		}
-		return true;
+		return key;
 	}
 
 	public static void main(String[] args) {
@@ -16,13 +18,15 @@ public class ComparingNew {
 		int[] array1 = { 1, 2, 3, 4 };
 		int[] array2 = { 1, 4, 3, 4 };
 
-		if (array1.length == array2.length) {
+		if (array1.length != array2.length) {
 			Arrays.sort(array1);
 			Arrays.sort(array2);
-		} else if (compareArrays(array1, array2)) {
-			System.out.println("Arrays are equal");
 		} else {
-			System.out.println("Arrays not equal");
+			if (compareArrays(array1, array2)) {
+				System.out.println("Arrays are equal");
+			} else {
+				System.out.println("Arrays not equal");
+			}
 		}
 
 		// System.out.println(array1.length == array2.length);
@@ -30,7 +34,7 @@ public class ComparingNew {
 		Arrays.sort(array1);
 		Arrays.sort(array2);
 
-		System.out.println(compareArrays(array1, array2));
+		// System.out.println(compareArrays(array1, array2));
 
 		// for (int number : array1) {
 		// System.out.println("Number = " + number);
