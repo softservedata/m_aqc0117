86a420644172efb0b7956807a15d6a2392e85eab
test <test@gmail.com>
2017-02-15 09:35:34 +0200

    Add JUnit and TestNG Examples

---
+++ src/com/softserve/edu/Calc.java
@@ -0,0 +1,14 @@
+package com.softserve.edu;
+
+public class Calc {
+
+	public double add(int arg0, int arg1) {
+		return arg0 + arg1;
+	}
+
+	//public double div(int arg0, int arg1) {
+	public double div(double arg0, double arg1) {
+		return arg0 / arg1;
+	}
+
+}
