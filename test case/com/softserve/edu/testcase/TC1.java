7f5e76172e01424a7edb3149028a38251fe4db17
npylyp <npylyp@GUMMIES.softservecom.com>
2017-02-17 10:26:58 +0200

    Test Case 1

---
+++ test case/com/softserve/edu/testcase/TC1.java
@@ -0,0 +1,41 @@
+package com.softserve.edu.testcase;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC1 {
+
+	@Test
+	public void testTC1() throws Exception {
+		// Precondition
+		System.setProperty("webdriver.gecko.driver", "./lib/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://regres.herokuapp.com/login ");
+		Thread.sleep(1000);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		//************************
+		driver.findElement(By.id("login")).clear();
+		driver.findElement(By.id("login")).sendKeys("work");
+		
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("qwerty");
+		
+		driver.findElement(By.linkText("”‚≥ÈÚË")).click();
+				
+		driver.quit();
+		
+		//****************
+		
+		Thread.sleep(4000);
+		//driver.close();
+		driver.quit();
+	}
+
+}
